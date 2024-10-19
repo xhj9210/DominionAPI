93398eeb1d05182ec97c448fce530b7653b62f49
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-10-19 17:57:59 +0800

    update some api

---
+++ src/main/java/cn/lunadeer/dominion/events/PlayerMoveInDominionEvent.java
@@ -0,0 +1,12 @@
+package cn.lunadeer.dominion.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerMoveInDominionEvent extends Event {
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return null;
+    }
+}
---
+++ src/main/java/cn/lunadeer/dominion/events/PlayerMoveOutDominionEvent.java
@@ -0,0 +1,12 @@
+package cn.lunadeer.dominion.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerMoveOutDominionEvent extends Event {
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return null;
+    }
+}
