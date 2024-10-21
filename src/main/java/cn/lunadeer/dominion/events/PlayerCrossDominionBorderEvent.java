10e76150b809862daf0b77ae55bb4d2b322d6205
zhangyuheng <zhangyuheng@lunadeer.cn>
2024-10-21 10:07:43 +0800

    add PlayerCrossDominionBorderEvent comment

--- src/main/java/cn/lunadeer/dominion/events/PlayerCrossDominionBorderEvent.java
+++ src/main/java/cn/lunadeer/dominion/events/PlayerCrossDominionBorderEvent.java
@@ -11,6 +11,8 @@ import org.jetbrains.annotations.Nullable;
  * 玩家穿过领地边界事件，包含了（进入、离开、从一个领地移动到另一个领地）三种情况。
  * 当玩家从没有领地的位置移动到领地内部时 from 为 null，当玩家从领地内部移动到没有领地的位置时 to 为 null。
  * 从理论上来说，通常情况下：from 与 to 均可能为 null 但不会同时为 null，在均不为 null 的情况下 from 与 to 不会相同。
+ * 如果仅希望监听玩家进入领地的事件请参考 {@link PlayerMoveInDominionEvent}，
+ * 如果仅希望监听玩家离开领地的事件请参考 {@link PlayerMoveOutDominionEvent}。
  */
 public class PlayerCrossDominionBorderEvent extends Event {
 
